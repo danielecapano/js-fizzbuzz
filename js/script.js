ab5c0f5b90d90a082ce9c475c4fd2b93c7c7ee61
danielecapano <daniele.capano@gmail.com>
2023-10-06 16:25:39 +0200

    FIX - delete variables sconto3 and sconto5

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@ const gridElDOM = document.querySelector(".grid");
 
 for (let i = 0; i < 100; i++) {
   let n = i + 1;
-  const resto3 = n % 3;
-  const resto5 = n % 5;
+
   const boxElDOM = document.createElement("div");
   boxElDOM.classList.add("box");
   gridElDOM.append(boxElDOM);
 
-  if (resto3 === 0 && resto5 === 0) {
+  if (n % 3 === 0 && n % 5 === 0) {
     n = "fizzbuzz";
     boxElDOM.classList.add("fizzbuzz");
-  } else if (resto3 === 0) {
+  } else if (n % 3 === 0) {
     n = "fizz";
     boxElDOM.classList.add("fizz");
-  } else if (resto5 === 0) {
+  } else if (n % 5 === 0) {
     n = "buzz";
     boxElDOM.classList.add("buzz");
   }
